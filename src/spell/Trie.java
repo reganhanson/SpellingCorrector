277f696933b36f9da19045957401972b055a3253
Regan Hanson <reganhanson1994@gmail.com>
2020-09-13 12:37:47 -0600

    Working-debug

--- src/spell/Trie.java
+++ src/spell/Trie.java
@@ -11,38 +11,38 @@ public class Trie implements ITrie {
     }
 
     @Override
-    /**
-     * Adds the specified word to the trie (if necessary) and increments the word's frequency count.
-     *
-     * @param word the word being added to the trie
-     */
     public void add(String word) {
+        word = word.toLowerCase();
         if (word.isEmpty()) {
             this.root.EOW = true;
             return;
         }
         Node currentNode = root;
+        //root.EOW = false;
         for (int i = 0; i < word.length(); i++) {
             if (currentNode.children[word.charAt(i) - 'a'] == null) {
                 // Create the node
                 currentNode.children[word.charAt(i) - 'a'] = new Node();
                 nodeCount++;
                 // make that node the current Node
-                currentNode = currentNode.children[word.charAt(i) - 'a'];
                 // move along
             }
-            else {
-                currentNode = currentNode.children[word.charAt(i) - 'a'];
-            }
+            currentNode = currentNode.children[word.charAt(i) - 'a'];
         }
         currentNode.incrementValue();
-        wordCount++;
+        if (currentNode.wordFrequency <= 1) {
+            wordCount++;
+        }
     }
 
     @Override
     public INode find(String word) {
+        word = word.toLowerCase();
         // We always want to start at the root node
         Node currentNode = root;
+        if (currentNode == null) {
+            return null;
+        }
         for (int i = 0; i < word.length(); i++) {
             if (currentNode.children[word.charAt(i) - 'a'] == null) {
                 return null;
